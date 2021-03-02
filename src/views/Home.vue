b6c271a1ad1cefeaa8753aafe259305e5dd3328b
sarah <sarahaleesia1@gmail.com>
2021-03-02 14:35:26 -0600

    home page/post index

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,40 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
-    <div>
-      v-for = post in posts
+    <div v-for="post in posts" v-bind:key="post.id">
+      <h1>{{ post.title }}</h1>
+      <p>{{ post.body }}</p>
+      <p>{{ post.created_by["name"] }}</p>
+      <p>{{ post.created_at }}</p>
     </div>
   </div>
 </template>
 <style></style>
 
 <script>
+import axios from "axios";
 export default {
   data: function() {
     return {
-      message: "Welcome to Vue.js!",
+      message: "Daily Affirmation App",
       posts: [],
     };
   },
   created: function() {
-    axios.get("/api/posts").then(response) => console.log(response.data));
-    this.posts = response.data;
+    this.indexPosts();
   },
-  methods: {},
+  methods: {
+    indexPosts: function() {
+      axios
+        .get("/api/posts")
+        .then((response) => {
+          console.log(response.data);
+          this.posts = response.data;
+        })
+        .catch((error) => {
+          console.log(error.response.data.errors);
+        });
+    },
+  },
 };
 </script>
