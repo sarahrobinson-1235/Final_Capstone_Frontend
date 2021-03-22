9493869bc0a11cf6a2f11ac4f9d120514106b19d
sarah <sarahaleesia1@gmail.com>
2021-03-21 21:36:41 -0500

    implements theme on Login page

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -139,36 +139,6 @@
       <div class="e-divider-4"></div>
     </div>
     <!-- /Content Sections -->
-
-    <!-- <form v-on:submit.prevent="submit()">
-      <h1>Signup</h1>
-      <ul>
-        <li class="text-danger" v-for="error in errors" v-bind:key="error">
-          {{ error }}
-        </li>
-      </ul>
-      <div class="form-group">
-        <label>Name:</label>
-        <input type="text" class="form-control" v-model="name" />
-      </div>
-      <div class="form-group">
-        <label>Email:</label>
-        <input type="email" class="form-control" v-model="email" />
-      </div>
-      <div class="form-group">
-        <label>Password:</label>
-        <input type="password" class="form-control" v-model="password" />
-      </div>
-      <div class="form-group">
-        <label>Password confirmation:</label>
-        <input
-          type="password"
-          class="form-control"
-          v-model="passwordConfirmation"
-        />
-      </div>
-      <input type="submit" class="btn btn-primary" value="Submit" />
-    </form> -->
   </div>
 </template>
 
